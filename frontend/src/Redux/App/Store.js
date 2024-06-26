3185a47f6faa25ee23247313268b3b8f980af6b9
Dhrujal Navadiya <dhruvnavadiya14@gmail.com>
2024-06-26 16:33:21 +0530

    remove redux

--- frontend/src/Redux/App/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import productReducer from "../feature/ProductSlice";
-
-export const store = configureStore({
-  reducer: {
-    allCart: productReducer,
-  },
-});
